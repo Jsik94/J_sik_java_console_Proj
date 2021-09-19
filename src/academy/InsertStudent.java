223776054bf2da0cd7d05b9be64532babe966aaa
JSik <31975036+ychic@users.noreply.github.com>
2021-09-19 22:57:40 +0900

    Patternize Insert,Print class

---
+++ src/academy/InsertStudent.java
@@ -0,0 +1,58 @@
+package academy;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class InsertStudent implements Insert ,CodeInfo{
+
+    final String TITLE = "학생";
+    InputClass ip;
+    Person newOne;
+    Map<String, ArrayList<Person>> database;
+
+    public InsertStudent(Map<String, ArrayList<Person>> database) {
+        this.database = database;
+    }
+
+    @Override
+    public int insert() {
+
+        ip = new InputClass();
+
+
+        System.out.println("["+TITLE+" 필수 정보 입력란]");
+        String[] data = new String[Student.cols.length];
+        for (int i = 0 ; i < Student.cols.length ; i++){
+
+
+            System.out.println(TITLE +"의 "+Student.cols[i]+"을(를) 입력하세요");
+            if(Student.cols[i].equals("나이") ||Student.cols[i].equals("학번") ){
+                data[i] = Integer.toString(ip.getInteger());
+            } else{
+                data[i] = ip.getString();
+            }
+
+        }
+
+        newOne = new Student.StudentBuilder(Integer.parseInt(data[0]),data[1],data[2])
+                .setAddr(data[3])
+                .setTel(data[4])
+                .setEmail(data[5])
+                .build();
+
+
+
+        String tag =MyUtil.getTag(newOne.getName());
+
+        if (database.getOrDefault(tag,null)==null){
+            database.put(tag,new ArrayList<>());
+        }
+
+        (database.get(tag)).add(newOne);
+
+
+
+        return CURRENT;
+
+    }
+}
---
+++ src/academy/InsertTeacher.java
@@ -0,0 +1,58 @@
+package academy;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class InsertTeacher implements Insert ,CodeInfo{
+
+    final String TITLE = "선생님";
+    InputClass ip;
+    Person newOne;
+    Map<String, ArrayList<Person>> database;
+
+    public InsertTeacher(Map<String, ArrayList<Person>> database) {
+        this.database = database;
+    }
+
+    @Override
+    public int insert() {
+        ip = new InputClass();
+
+
+        System.out.println("["+TITLE+" 필수 정보 입력란]");
+        String[] data = new String[Teacher.cols.length];
+        for (int i = 0 ; i < Teacher.cols.length ; i++){
+
+
+            System.out.println(TITLE +"의 "+ Teacher.cols[i]+"을(를) 입력하세요");
+            if(Teacher.cols[i].equals("나이")){
+                data[i] = Integer.toString(ip.getInteger());
+            }
+
+            else{
+                data[i] = ip.getString();
+            }
+
+        }
+
+        newOne = new Teacher.TeacherBuilder(Integer.parseInt(data[0]),data[1],data[2])
+                .setAddr(data[3])
+                .setTel(data[4])
+                .setEmail(data[5])
+                .build();
+
+
+        String tag =MyUtil.getTag(newOne.getName());
+
+        if (database.getOrDefault(tag,null)==null){
+            database.put(tag,new ArrayList<>());
+        }
+
+        (database.get(tag)).add(newOne);
+
+
+        return CURRENT;
+    }
+    
+    
+}
